54b6ffeaf02f3752eae90a65c0be71005af334a1
Peter <mrkisspeter@gmail.com>
2021-03-18 23:17:34 +0100

    Initial commit

---
+++ cookbook-app/src/main/java/cookbook/App.java
@@ -0,0 +1,10 @@
+package cookbook;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class App {
+    public void start(){
+        System.out.println("Hello Jani!");
+    }
+}
---
+++ cookbook-app/src/main/java/cookbook/SpringBootApp.java
@@ -0,0 +1,25 @@
+package cookbook;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootApp {
+
+    @Autowired
+    private App app;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> app.start();
+    }
+
+}
